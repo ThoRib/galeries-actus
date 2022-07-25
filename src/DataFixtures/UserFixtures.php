150275b41fc3c6a45778bcadb22f7c0930399f4e
ThoRib <thomas.ribardiere@wanadoo.fr>
2022-07-25 22:24:11 +0200

    Crea User avec Username

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserFixtures extends Fixture
+{
+    private $encoder;
+
+    public function __construct(UserPasswordHasherInterface $userPasswordHascherInterface)
+    {
+        $this->encoder = $userPasswordHascherInterface;        
+    }
+    public function load(ObjectManager $manager): void
+    {
+        $user = new User();
+        $manager->persist($user);
+        $user->setUsername('thomas');
+        $user->setRoles(["ROLE_USER","ROLE_ADMIN"]);
+        $user->setPassword($this->encoder->hashPassword($user, "mdp"));
+
+        $manager->flush();
+    }
+}
