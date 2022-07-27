d84a6e42388b678ebf9682b2361c1ae96d97d050
ThoRib <thomas.ribardiere@wanadoo.fr>
2022-07-27 21:18:56 +0200

    Création et modification user et admin dans le CRUD

--- src/Controller/AdminUserController.php
+++ src/Controller/AdminUserController.php
@@ -5,10 +5,11 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\UserType;
 use App\Repository\UserRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 /**
  * @Route("/admin/user")
@@ -28,13 +29,28 @@ class AdminUserController extends AbstractController
     /**
      * @Route("/new", name="app_admin_user_new", methods={"GET", "POST"})
      */
-    public function new(Request $request, UserRepository $userRepository): Response
+    public function new(Request $request, UserRepository $userRepository, UserPasswordHasherInterface $userPasswordHasherInterface): Response
     {
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+        
+            //Ajout de la prise en charge des rôles
+            if($request->request->get("user")["role"]=="ADMIN") {
+                $user->setRoles(["ROLE_USER", "ROLE_ADMIN"]);
+            } else {
+                $user->setRoles(["ROLE_USER"]);
+            }
+
+            //Ajout de la prise en charge du mot de passe
+            $hashedPassword = $userPasswordHasherInterface->hashPassword($user, $request->request->get("user")["plainPassword"]);
+
+            // On affecte la valeur du mot de passe hashé à la propriété password de l'utilisateur
+            $user->setPassword($hashedPassword);
+
+            //Mise en BDD en utilisant la méthode originale
             $userRepository->add($user, true);
 
             return $this->redirectToRoute('app_admin_user_index', [], Response::HTTP_SEE_OTHER);
@@ -59,17 +75,32 @@ class AdminUserController extends AbstractController
     /**
      * @Route("/{id}/edit", name="app_admin_user_edit", methods={"GET", "POST"})
      */
-    public function edit(Request $request, User $user, UserRepository $userRepository): Response
+    public function edit(Request $request, User $user, UserRepository $userRepository, UserPasswordHasherInterface $userPasswordHasherInterface): Response
     {
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $userRepository->add($user, true);
 
+            //Ajout de la prise en charge des rôles
+            if($request->request->get("user")["role"]=="ADMIN") {
+                $user->setRoles(["ROLE_USER", "ROLE_ADMIN"]);
+            } else {
+                $user->setRoles(["ROLE_USER"]);
+            }
+          
+            if(!empty($request->request->get("user")["plainPassword"])) {
+                //Ajout de la prise en charge du mot de passe
+                $hashedPassword = $userPasswordHasherInterface->hashPassword($user, $request->request->get("user")["plainPassword"]);
+
+                // On affecte la valeur du mot de passe hashé à la propriété password de l'utilisateur
+                $user->setPassword($hashedPassword);
+            }
+
+            $userRepository->add($user, true);
             return $this->redirectToRoute('app_admin_user_index', [], Response::HTTP_SEE_OTHER);
         }
-
+        
         return $this->renderForm('admin_user/edit.html.twig', [
             'user' => $user,
             'form' => $form,
