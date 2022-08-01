a092cd5b73478a8039c13b34d674c1fb01e74f32
ThoRib <thomas.ribardiere@wanadoo.fr>
2022-08-01 20:21:59 +0200

    Crea formulaire enregistrement user

--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -48,6 +48,11 @@ class AppAuthenticator extends AbstractLoginFormAuthenticator
         if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName)) {
             return new RedirectResponse($targetPath);
         }
+        if(in_array("ROLE_ADMIN", $token->getUser()->getRoles())){
+            return new RedirectResponse($this->urlGenerator->generate('app_admin_galerie_index'));
+        } else {
+            return new RedirectResponse($this->urlGenerator->generate('app_accueil'));
+        }
 
         // For example:
         // return new RedirectResponse($this->urlGenerator->generate('some_route'));
